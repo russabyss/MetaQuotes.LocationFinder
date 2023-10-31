6fdefb626eeb418ac771f2000438bd32df789129
Ivan Kharchenko <monnok@live.com>
2023-10-31 22:53:08 +0300

    feat: added benchmarks for file loading

---
+++ tests/MetaQuotes.LocationFinder.Benchmarks/DbReaderHelperBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using MetaQuotes.LocationFinder.Core.Helpers;
+
+namespace MetaQuotes.LocationFinder.Benchmarks
+{
+    public class DbReaderHelperBenchmark
+    {
+        private const string FilePath = "Data/geobase.dat";
+
+        /// <summary>
+        /// Чтение файла с диска.
+        /// </summary>
+        [Benchmark]
+        public void FileReadSimple()
+        {
+            _ = File.ReadAllBytes(FilePath);
+        }
+
+        /// <summary>
+        /// Чтение файла с диска с парсингом данных.
+        /// </summary>
+        [Benchmark]
+        public void FileReadWithParsing()
+        {
+            _ = DbReaderHelper.LoadFromFile(FilePath);
+        }
+
+    }
+}
---
+++ tests/MetaQuotes.LocationFinder.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+using MetaQuotes.LocationFinder.Benchmarks;
+
+//| Method               | Mean       | Error      | StdDev    |
+//| -------------------- | ----------:| ----------:| ---------:|
+//| FileReadSimple       | 5.057 ms   | 0.0727 ms  | 0.0645 ms |
+//| FileReadWithParsing  | 99.374 ms  | 1.6648 ms  | 1.8504 ms |
+
+var summary = BenchmarkRunner.Run<DbReaderHelperBenchmark>();
